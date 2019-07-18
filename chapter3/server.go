132984e94a57ed53578dd63b82ae046a1681bb35
spectrex02 <iscale821@gmail.com>
2019-07-18 22:57:53 +0900

    2019-07-17.1

--- chapter3/server.go
+++ chapter3/server.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
+	"time"
 
 	"github.com/k0kubun/pp"
 )
@@ -92,6 +93,46 @@ func thubmnailHndler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 }
 
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	//このエンドポイントでは変更以外は受け付けない
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to My protocol")
+
+	//低層のソケットを取得
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer conn.Close()
+
+	//プロトコルが変わるというレスポンスを送信
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	//オリジナルの通信の開始
+	for i := 0; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush() //Trigger "chunked" encoding and send a chunk...
+		recv, err := readWriter.ReadBytes('\n')
+		if err != nil {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
 //echo server
 func main() {
 	var httpServer http.Server
@@ -99,6 +140,7 @@ func main() {
 	http.HandleFunc("/cookie", cookieHandler)
 	http.HandleFunc("/digest", handlerDigest)
 	http.HandleFunc("/img", thubmnailHndler)
+	http.HandleFunc("/upgrade", handlerUpgrade)
 	log.Printf("start http listening:18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
